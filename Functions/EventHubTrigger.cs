ec41b15d198ae0d864a4bc1ca6488396494092a1
Alexander Nikiforov <alexander.nikiforov@gmail.com>
2020-01-12 20:49:26 +0100

    added event hub trigger

---
+++ Functions/EventHubTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace Functions
+{
+    public static class EventHubTrigger
+    {
+        [FunctionName("EventHubTrigger")]
+        public static async Task RunAsync(
+            [EventHubTrigger("evh-alnitest-dev-001", Connection = "EVENT_HUB_CONNECTION_STRING")]
+            string myEventHubMessage, ILogger log)
+        {
+            log.LogInformation($"C# Event Hub trigger function processed a message: {myEventHubMessage}");
+        }
+    }
+}
