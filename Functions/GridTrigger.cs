64e2d830164e71a2466badf6cb3b52a8d2ec9458
Alexander Nikiforov <alexander.nikiforov@gmail.com>
2020-01-12 19:38:57 +0100

    added a trigger for grid evens

---
+++ Functions/GridTrigger.cs
@@ -0,0 +1,20 @@
+﻿// Default URL for triggering event grid function in the local environment.
+// http://localhost:7071/runtime/webhooks/EventGrid?functionName=GridTrigger
+
+using System.Threading.Tasks;
+using Microsoft.Azure.EventGrid.Models;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.EventGrid;
+using Microsoft.Extensions.Logging;
+
+namespace Functions
+{
+    public static class GridTrigger
+    {
+        [FunctionName("GridTrigger")]
+        public static async Task RunAsync([EventGridTrigger] EventGridEvent eventGridEvent, ILogger log)
+        {
+            log.LogInformation(eventGridEvent.Data.ToString());
+        }
+    }
+}
